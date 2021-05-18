b179a883ade7286fe8715688c7776d7e4d9907cf
Skyline-23 <bs2740@naver.com>
2021-05-18 16:51:26 +0900

    [ADD] API keys

---
+++ fine-dust/keys.swift
@@ -0,0 +1,13 @@
+//
+//  keys.swift
+//  
+//
+//  Created by 김부성 on 2021/05/18.
+//
+
+import Foundation
+
+class keys {
+    let airKoreaKey = "mUoz7fgLI+xTi0NVacA3owoO515glwvj8QBYiaVbmeFdu7oLFBk0quxN0jN2wjlMEPlOxKUepRtv2m8dJS9q8Q=="
+    let kakaoKey = "KakaoAK 41b8e7df68905f6788749d919520d22f"
+}
